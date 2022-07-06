1677169aa3e0899c36c65eeae458a22fffad4cbe
Andrey <andrey.y.n.1988@gmail.com>
2022-07-06 18:55:50 +0300

    solution

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -15,21 +15,22 @@ func getOne(id int64) user {
 }
 
 func getBatch(n int64, pool int64) (res []user) {
-	ch := make(chan int, pool)
+	var ch = make(chan int, pool)
 	var wg sync.WaitGroup
 	var mu sync.Mutex
 	var users []user
-	for i := 0; i < int(n); i++ {
+	var i int64
+	for i = 0; i < n; i++ {
 		wg.Add(1)
 		ch <- 1
-		go func(j int, u []user) {
+		go func(j int64) {
 			defer wg.Done()
 			defer mu.Unlock()
-			itemUser := getOne(int64(j))
+			itemUser := getOne(j)
 			mu.Lock()
 			users = append(users, itemUser)
 			<-ch
-		}(i, users)
+		}(i)
 	}
 	wg.Wait()
 	return users
